aa3f28d4f4fc9d4f729f100e9c4f7042933eaa28
YoheiMiyamoto <yoheimiyamoto@gmail.com>
2019-01-12 17:38:46 +0900

    update

--- simpletree/client.go
+++ simpletree/client.go
@@ -6,7 +6,27 @@ import (
 	"path/filepath"
 )
 
-var depth int
+/*
+true = last
+*/
+type Depth []bool
+
+func (d Depth) add(b bool) Depth {
+	return append(d, b)
+}
+
+func (d Depth) remove() Depth {
+	if len(d) == 0 {
+		return d
+	}
+	return d[0 : len(d)-1]
+}
+
+func (d Depth) int() int {
+	return len(d)
+}
+
+var depth = make(Depth, 0)
 
 func dirWalk(dir string) error {
 	files, err := ioutil.ReadDir(dir)
@@ -15,21 +35,27 @@ func dirWalk(dir string) error {
 	}
 
 	for i, f := range files {
+		// fmt.Println(depth)
 		if f.IsDir() {
-			fmt.Printf("%*s%s\n", depth*4, "", f.Name())
-			path := filepath.Join(dir, f.Name())
-			depth++
-			dirWalk(path)
-			depth--
-		} else {
-			if i == 0 {
-				fmt.Println(print(f.Name(), depth, TypeFirst))
-				// fmt.Printf("%*s%s!\n", depth*4, "", f.Name())
-			} else if i == len(files)-1 {
-				fmt.Println(print(f.Name(), depth, TypeEnd))
+			// fmt.Printf("%*s%s\n", depth.int()*4, "", f.Name())
+			fmt.Printf("%s, %s\n", dir, print(f.Name(), depth))
+			if i == len(files)-1 {
+				depth = depth.add(true)
 			} else {
-				fmt.Println(print(f.Name(), depth, TypeMiddle))
+				depth = depth.add(false)
 			}
+			dirWalk(filepath.Join(dir, f.Name()))
+			depth = depth.remove()
+		} else {
+			fmt.Printf("%s, %s\n", dir, print(f.Name(), depth))
+			// if i == 0 {
+			// 	fmt.Println(print(f.Name(), depth))
+			// 	// fmt.Printf("%*s%s!\n", depth*4, "", f.Name())
+			// } else if i == len(files)-1 {
+			// 	fmt.Println(print(f.Name(), depth))
+			// } else {
+			// 	fmt.Println(print(f.Name(), depth))
+			// }
 			// fmt.Printf("%*s%s\n", depth*4, "", f.Name())
 		}
 	}
@@ -44,19 +70,21 @@ const (
 	TypeEnd
 )
 
-func print(name string, depth int, p PrintType) string {
+func print(name string, depth Depth) string {
 	var out string
-	for i := 0; i < depth-1; i++ {
-		out += "│   "
+	for i, d := range depth {
+		if i == len(depth)-1 {
+			if d {
+				out += "└──"
+			} else {
+				out += "├──"
+			}
+			continue
+		}
+		if !d {
+			out += "│   "
+		}
 	}
-	switch p {
-	case TypeFirst:
-		out += "└──"
-	case TypeMiddle:
-		out += "├──"
-	case TypeEnd:
-		out += "│  "
-	}
-	out += fmt.Sprintf(" %s", name)
+	out += fmt.Sprintf(" %s, depth: %v", name, depth)
 	return out
 }
